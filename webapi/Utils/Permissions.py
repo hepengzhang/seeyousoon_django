5c2dc1f6776fcca23c175a6438520f89e3a420f1
hepeng zhang <hz557@nyu.edu>
2013-12-09 15:23:32 -0500

    Added permission.

--- webapi/Utils/Permissions.py
+++ webapi/Utils/Permissions.py
@@ -1,5 +1,6 @@
 from rest_framework import permissions
 from django.contrib.auth.models import AnonymousUser
+from webapi import models
 
 class AuthPermission(permissions.BasePermission):
     """
@@ -7,4 +8,24 @@ class AuthPermission(permissions.BasePermission):
     """
     def has_permission(self, request, view):
         return type(request.user) != AnonymousUser
+    
+class PeoplePermission(AuthPermission):
+    """
+    Object-level permission that determines if given user is visible to current user
+    """
+    
+    def has_object_permission(self, request, view, obj):
+        requested_user_id = obj.user_id
+        current_user_id = request.user.user_id 
+        if requested_user_id == current_user_id:
+            return True
+        if request.method in permissions.SAFE_METHODS:
+            isFriend = models.friends.objects.filter(user_id=requested_user_id, friend_id=current_user_id, status__gt=0).count()
+            return isFriend == 1
+
+class ActivityPermission(AuthPermission):
+    
+    def has_object_permission(self, request, view, obj):
+        return True; 
+        
         
