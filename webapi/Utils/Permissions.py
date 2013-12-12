540a4144034b148047c4162a2c6ac9da4766a40f
hepeng zhang <hz557@nyu.edu>
2013-12-12 00:25:45 -0500

    Rename peoplePermission to isFriend

--- webapi/Utils/Permissions.py
+++ webapi/Utils/Permissions.py
@@ -9,7 +9,7 @@ class AuthPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         return type(request.user) != AnonymousUser
     
-class PeoplePermission(AuthPermission):
+class isFriend(AuthPermission):
     """
     Object-level permission that determines if given user is visible to current user
     """
@@ -22,6 +22,8 @@ class PeoplePermission(AuthPermission):
         if request.method in permissions.SAFE_METHODS:
             isFriend = models.friends.objects.filter(user_id=requested_user_id, friend_id=current_user_id, status__gt=0).count()
             return isFriend == 1
+
+    
 
 class ActivityPermission(AuthPermission):
     
