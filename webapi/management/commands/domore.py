13ea8832a31303f6c1ac21c75095df52dd9b42b7
hepeng zhang <hz557@nyu.edu>
2013-12-28 01:10:27 -0500

    Revised fulltext insertion "domore.py"

--- webapi/management/commands/domore.py
+++ webapi/management/commands/domore.py
@@ -1,20 +1,28 @@
 from django.core.management.base import BaseCommand, CommandError
-from django.db import connection, transaction
-import MySQLdb as database
-from warnings import filterwarnings
-
-filterwarnings('ignore', category=database.Warning)
+from django.db import connection
+import sys
 
 class Command(BaseCommand):
     args = ''
-    help = 'Drop database and create database'
+    help = 'Create FULLTEXT INDEX on user_search'
 
     def handle(self, *args, **options):
         try:
+            app_name = self.__module__.split('.')[0]
+            user_search_table = "{}_{}".format(app_name, 'user_search')
+            column_name = 'search_index'
+            index_name = "{}_{}_fulltext".format(user_search_table, column_name)
+            fulltext_create_query = "CREATE FULLTEXT INDEX `{}` ON `{}` ({});".format(index_name, user_search_table,
+                                                                                      column_name)
             c = connection.cursor()
-            fulltextindex = "CREATE FULLTEXT INDEX `webapi_user_search_index` ON `webapi_user_search` (username, name);"
-            c.execute(fulltextindex)
-            transaction.commit_unless_managed()
-            self.stdout.write("FULLTEXT INDEX on user_search created")
+            try:
+                c.execute(fulltext_create_query)
+                print fulltext_create_query
+            except Warning as w:
+                print "Warning: " + str(w)
+            except Exception as e:
+                print "Unexpected error:", str(e)
+            except:
+                print "Unexpected error:", sys.exc_info()[0]
         except Warning as e:
-            raise CommandError(str(type(e))+str(e))
+            raise CommandError(str(type(e)) + str(e))
